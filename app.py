c904640e19cd44f00269e0e206c32e4141ccae0d
educifuentes <edu.cifuentes@gmail.com>
2026-01-19 19:02:06 -0300

    feat: Add a "Guidelines" page

--- app.py
+++ app.py
@@ -23,12 +23,13 @@ documentation_page = st.Page("pages/tools/3_documentation.py", title="Documentat
 
 style_page = st.Page("pages/guides/1_style.py", title="Style", icon=":material/palette:")
 features_page = st.Page("pages/guides/2_features.py", title="Features", icon=":material/assignment_turned_in:")
+guidelines_page = st.Page("pages/guides/3_guidelines.py", title="Guidelines", icon=":material/assignment_turned_in:")
 
 # current page
 pg = st.navigation({
     "Reports": [overview_page, details_page],
     "Tools": [uploads_page, explore_page, documentation_page],
-    "Guides": [style_page, features_page]
+    "Guides": [style_page, features_page, guidelines_page]
 })
 
 # ==========================================
---
+++ pages/guides/3_guidelines.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+
+st.title("Guidelines")
+
+st.header("Usar yaml para definir como mostrar los datos")
+
+st.markdown(""" multi catageory, dates, amounts, 
+
+""")
+
