adcb75536df17e4e88e29d12b950a7e99f3db673
educifuentes <edu.cifuentes@gmail.com>
2026-01-19 18:42:39 -0300

    docs: remove duplicate subheader and add sections for frequently used icons and colors to the style guide.

--- pages/guides/1_style.py
+++ pages/guides/1_style.py
@@ -1,7 +1,6 @@
 import streamlit as st
 
 st.title("Style")
-st.subheader("Style")
 
 st.write("This is a style guide for the app.")
 
@@ -9,3 +8,15 @@ st.subheader("Icons")
 st.markdown("use material icon fron [Material Symbols & Icons - Google Fonts](https://fonts.google.com/icons?selected=Material+Symbols+Outlined:book_2:FILL@0;wght@400;GRAD@0;opsz@24&icon.query=documentation&icon.size=24&icon.color=%23e3e3e3)")
 
 st.code("use names of icon for example :material/dashboard:")
+
+st.subheader("Usados frecuentemente")
+
+favorites = '''
+assignment_turned_in
+dashboard
+inventory_2
+'''
+st.code(favorites)
+
+st.subheader("Colors")
+st.markdown("Look up in Colorhunt website and use hex color")
