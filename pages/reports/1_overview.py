b8d74119b2039a6bd4948e91132fe1b4855c6d33
educifuentes <edu.cifuentes@gmail.com>
2026-01-19 18:33:58 -0300

    feat: Add introductory markdown message, deployment links, and a 'Data' subheader to the overview page.

--- pages/reports/1_overview.py
+++ pages/reports/1_overview.py
@@ -5,9 +5,20 @@ from datetime import datetime
 st.title("Sample Dashboard")
 st.subheader("Overview")
 
+
+st.markdown("Hello! este es una data app de prueba y knowledge base")
+
+st.markdown("Deploy en [Streamlit Cloud Link](https://spendee-dashboard-educifuentes.streamlit.app/)")
+st.markdown("Deploy en [Render Link](https://spendee-dashboard-educifuentes.streamlit.app/)")
+st.markdown("Deploy en [Heroku Link](https://spendee-dashboard-educifuentes.streamlit.app/)")
+
+
+st.subheader("Data")
+
 dataframe = pd.DataFrame({
     "first column": [1, 2, 3, 4],
     "second column": [10, 20, 30, 40],
 })
 
 st.dataframe(dataframe)
+
